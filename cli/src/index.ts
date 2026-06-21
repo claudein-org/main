17694105f3e9769f292b1d44bc9c0d69a11bad48
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-06-21 09:36:27 +0300

    fix

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,10 +1,14 @@
+#!/usr/bin/env node
 import { createServer } from 'http'
 import { AddressInfo, WebSocketServer } from 'ws'
 
 import { cli, command } from '@versecafe/zcli'
+import open from 'open'
 
 const start = command('start')
   .action(async ({ }) => {
+    const DEV = process.env.CIN_ENV === 'dev'
+    console.log(`Starting ${DEV ? 'development' : 'production'} server...`)
     const server = createServer()
     const wss = new WebSocketServer({ noServer: true })
 
@@ -17,7 +21,11 @@ const start = command('start')
 
     server.listen(0, () => {
       const { port } = server.address() as AddressInfo
-      console.log(`Listening on port ${port}`)
+      const url = DEV
+        ? `https://localhost:3000`
+        : 'https://claudein.org'
+
+      open(url)
     })
   })
 
