c22ee41249f6c5cde55b4c44a622c2b4633d3186
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-06-22 13:26:11 +0300

    fix

--- web/component/Poster.tsx
+++ web/component/Poster.tsx
@@ -4,6 +4,7 @@ import { btn } from "@/css/style.css"
 import { app } from "@/lib/app"
 import { cx } from "@/styled-system/css"
 import { useEffect, useState } from "react"
+import Reload from "./Reload"
 
 const ONE_DAY_MS = 24 * 60 * 60 * 1000
 const ONE_HOUR_MS = 60 * 60 * 1000
@@ -24,12 +25,15 @@ export default function Poster({ port, expires_at }: Props) {
 
     const needsReconnect = !expires_at || (expires_at * 1000 - now) < ONE_DAY_MS
 
-    if (needsReconnect) return <a
-        className={cx(btn({ color: 'dark' }))}
-        target="_blank"
-        href={app.linkedin}>
-        Connect To LinkedIn
-    </a>
+    if (needsReconnect) return <>
+        <Reload />
+        <a
+            className={cx(btn({ color: 'dark' }))}
+            target="_blank"
+            href={app.linkedin}>
+            Connect To LinkedIn
+        </a>
+    </>
 
     return <div>
         You can post to Linkedin
