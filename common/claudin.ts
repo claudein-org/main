0902ee631d2e0c0a128637e0cec4faf04385f549
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-06-22 14:56:11 +0300

    fix

--- common/claudin.ts
+++ common/claudin.ts
@@ -1,5 +1,7 @@
 import { createHash } from "crypto"
+import { writeFile } from "fs/promises"
 import z from "zod"
+import { zodToJsonSchema } from "zod-to-json-schema"
 
 const Image = z.object({
     src: z.string(),
@@ -8,6 +10,7 @@ const Image = z.object({
 
 export type Post = z.infer<typeof Post>
 const Post = z.object({
+    date: z.coerce.date(),
     text: z.string().optional(),
     images: z.array(Image).optional(),
 })
@@ -19,4 +22,9 @@ export const Claudin = z.object({
 export function hash(post: Post): bigint {
     const digest = createHash("sha256").update(JSON.stringify(post)).digest()
     return digest.readBigUInt64BE(0)
+}
+
+if (import.meta.main) {
+    // TODO: export schema
+    await writeFile('claudein.schema.json', zodToJsonSchema(Claudin, 'ClaudinSchema'))
 }
