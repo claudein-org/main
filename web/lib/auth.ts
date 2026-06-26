c7529a48a02e4464878f36e3a9af264d45bd1deb
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-06-26 12:52:27 +0300

    fix

--- web/lib/auth.ts
+++ web/lib/auth.ts
@@ -1,6 +1,6 @@
 export namespace auth {
     const PROD = process.env.NODE_ENV === 'production'
-    const DOMAIN = PROD ? 'claudein.org' : 'localhost:3000'
+    const DOMAIN = PROD ? 'claudein.org' : (process.env.DEV_DOMAIN ?? 'localhost:3000')
 
     export function getRedirectUri(provider: 'google' | 'linkedin' | 'facebook' | 'instagram' | 'youtube') {
         return `https://${DOMAIN}/auth/${provider}/`
