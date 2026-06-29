3d858aa923772218552060c156a3d9e596841a34
Gilad Kutiel <gilad.kutiel@gmail.com>
2026-06-29 14:40:11 +0300

    fix

---
+++ video/src/ClaudeLaughing.tsx
@@ -0,0 +1,3 @@
+export function ClaudeLaughing() {
+    // TODO: create an animated component that generates a laughing animation of the claude code logo, use this svg as a baseline claudecode-color.svg
+}
